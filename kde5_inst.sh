1e76eba4c7e4e3a3a6b01c11a45073e2da2ecdb4
RGreeneRI <22133496+RGreeneRI@users.noreply.github.com>
2018-12-23 20:37:41 -0500

    Update kde5_inst.sh

--- kde5_inst.sh
+++ kde5_inst.sh
@@ -36,110 +36,32 @@ echo ""
 # prompt user to press enter or not
 read -r -p "Press ENTER to continue, or Control-C to abort..." key
 
-clear;
-
-echo -e "${COOL}********************************************************************************"
-echo -e "********************************************************************************"
-echo -e "******                                                                    ******"
-echo -e "******   About to update FreeBSD                                          ******"
-echo -e "******                                                                    ******"
-echo -e "********************************************************************************"
-echo -e "********************************************************************************${NC}"
-echo ""
-echo ""
-
-# Start Countdown
-progress_bar() {
-  progress=$(( $1 * 30 / 5 ))
-  bar=`printf "\33[41m% ${progress}s\33[m" ""`
-  printf "\33[37D[%- 38s] %- 4s" "${bar}" "$1"
-}
-i=5
-printf "Proceeding in 5 seconds: \33[37C"
-while [ $i -ge 0 ];
-do
-  progress_bar $i
-  i=$(( $i - 1 ))
-  sleep 1
+# set vmware sysrc entries to yes or no
+echo -e "${Blue}Are you installing this on a VMware virtual machine?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) VM="YES"; break;;
+        No ) VM="NO"; break;;
+    esac
 done
-echo
-# End Countdown
 
 # fetch and install FreeBSD updates
 env PAGER=cat freebsd-update fetch;
 freebsd-update install;
 
-clear;
-
-echo -e "${COOL}********************************************************************************"
-echo -e "********************************************************************************"
-echo -e "******                                                                    ******"
-echo -e "******   About to set up the package manager and install packages.        ******"
-echo -e "******                                                                    ******"
-echo -e "********************************************************************************"
-echo -e "********************************************************************************${NC}"
-echo ""
-echo ""
-
-# Start Countdown
-progress_bar() {
-  progress=$(( $1 * 30 / 5 ))
-  bar=`printf "\33[41m% ${progress}s\33[m" ""`
-  printf "\33[37D[%- 38s] %- 4s" "${bar}" "$1"
-}
-i=5
-printf "Proceeding in 5 seconds: \33[37C"
-while [ $i -ge 0 ];
-do
-  progress_bar $i
-  i=$(( $i - 1 ))
-  sleep 1
-done
-echo
-# End Countdown
-
 # install package manager and install packages
 env ASSUME_ALWAYS_YES=yes pkg bootstrap;
-env ASSUME_ALWAYS_YES=yes pkg install wget nano sudo htop kde5 hal dbus xorg xterm bash zsh open-vm-tools xf86-video-vmware xf86-input-vmmouse sddm screenFetch chromium;
-
-clear;
-
-echo -e "${COOL}********************************************************************************"
-echo -e "********************************************************************************"
-echo -e "******                                                                    ******"
-echo -e "******   About to make changes to rc.conf                                 ******"
-echo -e "******                                                                    ******"
-echo -e "********************************************************************************"
-echo -e "********************************************************************************${NC}"
-echo ""
-echo ""
-
-# Start Countdown
-progress_bar() {
-  progress=$(( $1 * 30 / 5 ))
-  bar=`printf "\33[41m% ${progress}s\33[m" ""`
-  printf "\33[37D[%- 38s] %- 4s" "${bar}" "$1"
-}
-i=5
-printf "Proceeding in 5 seconds: \33[37C"
-while [ $i -ge 0 ];
-do
-  progress_bar $i
-  i=$(( $i - 1 ))
-  sleep 1
-done
-echo
-# End Countdown
+env ASSUME_ALWAYS_YES=yes pkg install wget nano sudo htop kde5 hal dbus xorg xterm bash zsh open-vm-tools xf86-video-vmware xf86-input-vmmouse sddm screenFetch octopkg chromium;
 
 # update rc.conf with required variables
 sysrc hald_enable="YES";
 sysrc dbus_enable="YES";
 sysrc sddm_enable="YES";
-sysrc vmware_guest_vmblock_enable="YES";
-sysrc vmware_guest_vmhgfs_enable="YES";
-sysrc vmware_guest_vmmemctl_enable="YES";
-sysrc vmware_guest_vmxnet_enable="YES";
-sysrc vmware_guestd_enable="YES";
+sysrc vmware_guest_vmblock_enable="$VM";
+sysrc vmware_guest_vmhgfs_enable="$VM";
+sysrc vmware_guest_vmmemctl_enable="$VM";
+sysrc vmware_guest_vmxnet_enable="$VM";
+sysrc vmware_guestd_enable="$VM";
 
 clear;
 
@@ -157,14 +79,14 @@ echo ""
 # prompt for username of regular user
 read -p 'Username: ' USERNAME
 
-# changer provided user's shell and set up empty ,zshrc file
+# changer provided user's shell and set up empty .zshrc user file
 chsh -s /usr/local/bin/zsh $USERNAME;
 cd /var/empty;
 cd /home/$USERNAME;
 echo "#Empty file, settings inherited from /usr/local/etc/zshrc" > .zshrc;
 chown $USERNAME:$USERNAME .zshrc;
 
-#Download customized zshrc from grml and customize further
+#Download customized zshrc from grml and customize terminal further
 cd /usr/local/etc;
 wget -O zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc;
 echo "screenFetch" >> zshrc;
@@ -184,7 +106,9 @@ clear;
 echo -e "${COOL}********************************************************************************"
 echo -e "********************************************************************************"
 echo -e "******                                                                    ******"
+echo -e "****** Installation Complete!                                             ******"
 echo -e "****** Machine will reboot in 20 seconds for the changes to take effect!  ******"
+echo -e "****** Press Ctrl-C to restart later.                                     ******"
 echo -e "******                                                                    ******"
 echo -e "********************************************************************************"
 echo -e "********************************************************************************${NC}"
