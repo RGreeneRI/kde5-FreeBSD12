777d7e11428eda76f8e43c6eb183b754bbcf1191
Rich Greene <22133496+RGreeneRI@users.noreply.github.com>
2019-10-06 18:51:34 -0400

    misc changes

--- kde5_inst.sh
+++ kde5_inst.sh
@@ -23,16 +23,26 @@ clear;
 
 echo -e "${COOL}********************************************************************************"
 echo -e "******   This script will install KDE Plasma and a bunch of useful stuff  ******"
-echo -e "******   to make FreeBSD fun to use!                                      ******"
+echo -e "******   to make FreeBSD easy fun to use!                                      ******"
 echo -e "********************************************************************************${NC}"
 echo ""
 echo ""
 
 # prompt user to press enter or not
-read -r -p "Press ENTER to continue, or Control-C to abort..." key
+#read -r -p "Press ENTER to continue, or Control-C to abort..." key
+echo -e "${Yellow}Would you like to proceed? Yes / No?"
+echo -e "${NC}"
+while true; do
+    read yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer Y or N.";;
+    esac
+done
 
 # set vmware sysrc entries to yes or no
-echo -e "${Blue}Are you installing this to a VMware virtual machine? Yes / No?"
+echo -e "${Yellow}Are you installing this to a VMware virtual machine? Yes / No?"
 echo -e "${NC}"
 while true; do
     read yn
@@ -43,6 +53,21 @@ while true; do
     esac
 done
 
+# prompt for username of regular user
+echo -e "${Yellow}Please type a username for a regular user.  If it doesn't exist, we'll create it"
+echo -e "${NC}"
+read -p 'Username: ' USERNAME
+
+# Check if user exists, if so, continue, if not force you to create
+USER_EXISTS=$(id -u $USERNAME > /dev/null 2>&1; echo $?)
+while [ $USER_EXISTS -eq 1 ]
+do
+  echo -e "${Yellow}$USERNAME doesn't exist, Creating User: $USERNAME.${NC}"
+  pw useradd -n $USERNAME -c "$USERNAME" -G wheel -s /bin/sh -m
+  echo -e "${Yellow}Type new password for $USERNAME...${NC}"
+  passwd $USERNAME
+  USER_EXISTS=$(id -u $USERNAME > /dev/null 2>&1; echo $?)
+done
 
 # fetch and install FreeBSD updates
 echo -e "${Yellow}Installing FreeBSD Updates${NC}"
@@ -52,10 +77,10 @@ freebsd-update install;
 # install package manager and install packages
 echo -e "${Yellow}Setting up package manager and installing packages${NC}"
 env ASSUME_ALWAYS_YES=yes pkg bootstrap;
-env ASSUME_ALWAYS_YES=yes pkg install wget nano sudo htop kde5 hal dbus xorg xterm bash zsh xf86-video-vmware xf86-input-vmmouse sddm screenFetch octopkg chromium;
+env ASSUME_ALWAYS_YES=yes pkg install wget nano sudo htop kde5 hal dbus xorg xterm bash zsh sddm screenFetch octopkg chromium;
 if [ "$VM" = "YES" ] ; then
-echo -e "${Yellow}Installing open-vm-tools package${NC}"
-env ASSUME_ALWAYS_YES=yes pkg install open-vm-tools;
+echo -e "${Yellow}Installing VM support packages and drivers${NC}"
+env ASSUME_ALWAYS_YES=yes pkg install open-vm-tools xf86-video-vmware xf86-input-vmmouse;
 fi
 
 # update rc.conf with required variables
@@ -72,33 +97,12 @@ sysrc vmware_guest_vmxnet_enable="$VM";
 sysrc vmware_guestd_enable="$VM";
 fi
 
-echo -e "${Yellow}************************************************************"
-echo -e "Please type in the username of your regular user to copy zsh"
-echo -e "config and set zsh as the default shell, and make a sudoer. "
-echo -e "If the provided username doesn't exist, it will be created. "
-echo -e "*************************************************************${NC}"
-echo ""
-echo ""
-
-# prompt for username of regular user
-read -p 'Username: ' USERNAME
-
-# Check if user exists, if so, continue, if not force you to create
-USER_EXISTS=$(id -u $USERNAME > /dev/null 2>&1; echo $?)
-while [ $USER_EXISTS -eq 1 ]
-do
-  echo -e "${Yellow}User: $USERNAME does not exist, Creating User: $USERNAME.${NC}"
-  pw useradd -n $USERNAME -c "$USERNAME" -G wheel -s /bin/sh -m
-  passwd $USERNAME
-USER_EXISTS=$(id -u $USERNAME > /dev/null 2>&1; echo $?)
-done
-
-# change provided user's shell, add to wheel for sudo access,  and set up empty .zshrc user file
-echo -e "${Yellow}Changing shell to zsh${NC}"
+# change provided user's shell, add to wheel for sudo access, and set up empty .zshrc user file
+echo -e "${Yellow}Changing $USERNAME's shell to zsh${NC}"
 chsh -s /usr/local/bin/zsh $USERNAME;
-echo -e "${Yellow}Adding user: $USERNAME to group: wheel${NC}"
+echo -e "${Yellow}Adding $USERNAME to the wheel group${NC}"
 pw usermod -n $USERNAME -G wheel;
-echo -e "${Yellow}Creating user's empty zshrc file${NC}"
+echo -e "${Yellow}Creating $USERNAME's empty zshrc file${NC}"
 cd /home/$USERNAME;
 echo "#Empty file, settings inherited from /usr/local/etc/zshrc" > .zshrc;
 chown $USERNAME:$USERNAME .zshrc;
